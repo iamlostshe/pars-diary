c3bcaf45fc29c4aaa24f63893d823e1976ea9734
Иван <vanamelcikov7275@gmail.com>
2025-08-29 15:52:42 +0400

    upd: config

--- pars_diary/config.py
+++ pars_diary/config.py
@@ -4,41 +4,31 @@ from __future__ import annotations
 
 from zoneinfo import ZoneInfo
 
+from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
 from pydantic_settings import BaseSettings
 
 from .utils.pars import Parser
 
-# Настройки часового пояса
 TIMEZONE = ZoneInfo("Europe/Moscow")
 
-# USERS_DB_FILE = "users.json"  # noqa: ERA001
-# METRICS_DB_FILE = "metrics.json"  # noqa: ERA001
 
 class Config(BaseSettings):
     """Глобальные настройки бота.
 
     Подгружаются один раз при запуске бота из .env файла.
 
-    - tg_token: От какого бота будут происходить действия.
-    - admins_tg: Список ID администраторов бота.
-    - git_url: Ссылка на репозиторий с исходным кодом проекта.
-    - hf_token: Токен для Hugging Face LLM модели.
+    - bot_token: От какого бота будут происходить действия.
+    - admin_ids: Список ID администраторов бота.
     """
 
-    token_tg: str
-    admins_tg: str
-    git_url: str
+    bot_token: str
+    admin_ids: str
 
 config: Config = Config(_env_file=".env")
-config.admins_tg = list(map(int, config.admins_tg.split(",")))
+config.admin_ids: list[int] = [int(i) for i in config.admin_ids.split(",")]
 
 parser = Parser()
 
-# Настройки бота по умолчанию
-default = DefaultBotProperties(parse_mode="html")
-
-# TODO(@iamlostshe): Разобраться с базами данных:
-
-# users_db = UsersDataBase(Path(USERS_DB_FILE))  # noqa: ERA001
-# metrics = MetricsDatabase(Path(METRICS_DB_FILE), users_db)  # noqa: ERA001
+# Инициализируем бот
+bot = Bot(token=config.bot_token, default=DefaultBotProperties(parse_mode="html"))
