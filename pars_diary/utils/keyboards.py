ec0e08d4fe0687ca43ed254a1f5667876976d40d
Иван <vanamelcikov7275@gmail.com>
2025-08-31 01:44:04 +0400

    upd: consts

--- pars_diary/utils/keyboards.py
+++ pars_diary/utils/keyboards.py
@@ -4,41 +4,33 @@ from __future__ import annotations
 
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from pars_diary.config import parser
-
-
-async def not_auth_keyboard(land_code: str | None = None) -> InlineKeyboardMarkup:
-    """Если этот контент не доступен без авторизациия (клавиатура)."""
-    if land_code != "ru":
-        land_code = "en"
-
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text="Инструкция",
-                    url="https://telegra.ph/Instrukciya-po-registracii-v-bote-04-25",
-                ),
-            ],
+not_auth_keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text="Инструкция",
+                url="https://telegra.ph/Instrukciya-po-registracii-v-bote-04-25",
+            ),
         ],
-    )
+    ],
+)
+reg_0 = InlineKeyboardMarkup(
+    inline_keyboard=[[InlineKeyboardButton(text="Начнём!", callback_data="reg_0")]],
+)
+reg_2 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(
+                text="Инструкция",
+                url="https://telegra.ph/Instrukciya-po-registracii-v-bote-04-25",
+            ),
+        ],
+    ],
+)
 
 
-async def reg_0(land_code: str | None = None) -> InlineKeyboardMarkup:
+async def reg_1() -> InlineKeyboardMarkup:
     """Нулевая стадия регистрации."""
-    if land_code != "ru":
-        land_code = "en"
-
-    return InlineKeyboardMarkup(
-        inline_keyboard=[[InlineKeyboardButton(text="Начнём!", callback_data="reg_0")]],
-    )
-
-
-async def reg_1(land_code: str | None = None) -> InlineKeyboardMarkup:
-    """Нулевая стадия регистрации."""
-    if land_code != "ru":
-        land_code = "en"
-
     result = []
     regions = await parser.get_regions()
 
@@ -53,20 +45,3 @@ async def reg_1(land_code: str | None = None) -> InlineKeyboardMarkup:
         )
 
     return InlineKeyboardMarkup(inline_keyboard=result)
-
-
-async def reg_2(land_code: str | None = None) -> InlineKeyboardMarkup:
-    """Нулевая стадия регистрации."""
-    if land_code != "ru":
-        land_code = "en"
-
-    return InlineKeyboardMarkup(
-        inline_keyboard=[
-            [
-                InlineKeyboardButton(
-                    text="Инструкция",
-                    url="https://telegra.ph/Instrukciya-po-registracii-v-bote-04-25",
-                ),
-            ],
-        ],
-    )
